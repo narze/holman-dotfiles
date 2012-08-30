232de2446e0a321a61099c109175084ed89b524b
Manassarn Manoonchai <manassarn@gmail.com>
2012-08-30 20:10:43 +0700

    update my config

---
+++ zsh/config_narze.zsh
@@ -0,0 +1,35 @@
+# automatically enter directories without cd
+setopt auto_cd
+
+# vi mode
+bindkey -v
+bindkey "^F" vi-cmd-mode
+bindkey jj vi-cmd-mode
+
+# use incremental search
+bindkey "^R" history-incremental-search-backward
+
+# add some readline keys back
+bindkey "^A" beginning-of-line
+bindkey "^E" end-of-line
+
+# handy keybindings
+bindkey "^P" history-search-backward
+bindkey "^Y" accept-and-hold
+bindkey "^N" insert-last-word
+bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
+
+# automatically pushd
+setopt auto_pushd
+export dirstacksize=5
+
+# awesome cd movements from zshkit
+setopt AUTOCD
+setopt AUTOPUSHD PUSHDMINUS PUSHDSILENT PUSHDTOHOME
+setopt cdablevars
+
+# expand functions in the prompt
+setopt prompt_subst
+
+# Try to correct command line spelling
+setopt CORRECT CORRECT_ALL
